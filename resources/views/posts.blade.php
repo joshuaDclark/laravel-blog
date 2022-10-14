5aca5e0c89b5c8b046bf0924ca08ffb267e3fa3c
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-10-14 15:57:12 -0400

    added model post and updated routes for id in place of slug

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
 
        <h1>
 
-            <a href="/posts/{{ $post->slug }}">
+            <a href="/posts/{{ $post->id }}">
         
             {{ $post->title }}
     
--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,9 @@ Route::get('/', function () {
 
   
 
-Route::get('posts/{post}', function ($slug) {
+Route::get('posts/{post}', function ($id) {
     return view('post', [
-        'post' => Post::find($slug)
+        'post' => Post::find($id)
    ]);
    
     
