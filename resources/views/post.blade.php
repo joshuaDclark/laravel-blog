d67872647d2c9e77382f7d9c1674c4a703a92444
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-10-14 14:53:20 -0400

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+
+<link rel="stylesheet" href="/app.css">
+<title>My Blog</title>
+
+
+
+<body>
+    <article>
+
+
+    <h1> {{ $post->title }}</h1>       
+
+    <div>
+        
+        {{!! $post->body !!}}
+
+    </div>
+
+     
+
+    </article>
+
+    <a href="/">Go Back</a>
+
+</body>
+
+</html>
