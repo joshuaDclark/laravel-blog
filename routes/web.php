67457fde3f7be0f634f5ffbf48aec77d8efe65df
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-10-17 15:40:43 -0400

    add functionality to view posts by author username

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::latest()->with('category', 'author')->get()
 
     ]);
 
@@ -41,4 +42,14 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'posts' => $category->posts
    ]);
    
+
+});
+
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
+   ]);
+   
+
 });
