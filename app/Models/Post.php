1c8a4e48eda717bcf0fb8003e1270f7569178c52
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-10-17 14:36:16 -0400

    added seeders for dev environment testing and added factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,6 +15,11 @@ class Post extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }  
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }    
 }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        $user = User::factory()->create([
+            'name' => 'John Doe'
+        ]);
+
+
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
+
+        // 
+
+        // $personal = Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
         // ]);
+
+
+        // $family =Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work'
+        // ]);
+
+        // $work = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family'
+        // ]);
+
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $family->id,
+        //     'title' => 'My Family Post',
+        //     'slug' => 'my-first-post',
+        //     'excerpt' => 'Lorem ipsum dolar sit amet.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Proin libero nunc consequat interdum varius. Ut placerat orci nulla pellentesque dignissim enim sit amet venenatis. Fames ac turpis egestas maecenas pharetra convallis. Quam adipiscing vitae proin sagittis nisl rhoncus. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar.</p> '
+        // ]);
+
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $work->id,
+        //     'title' => 'My Work Post',
+        //     'slug' => 'my-work-post',
+        //     'excerpt' => 'Lorem ipsum dolar sit amet.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Proin libero nunc consequat interdum varius. Ut placerat orci nulla pellentesque dignissim enim sit amet venenatis. Fames ac turpis egestas maecenas pharetra convallis. Quam adipiscing vitae proin sagittis nisl rhoncus. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar.</p> '
+        // ]);
+
     }
 }
