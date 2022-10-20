2d963414a8e66e89aa7d69acb9ae84621593a7de
Joshua Clark <joshua.clark@HO-L6402.local>
2022-10-20 13:56:14 -0400

    updated all image file paths

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -4,7 +4,7 @@
                     class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
                     <div class="py-6 px-5">
                         <div>
-                            <img src="./images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
+                            <img src="/images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
                         </div>
 
                         <div class="mt-8 flex flex-col justify-between">
@@ -15,11 +15,11 @@
 
                                 <div class="mt-4">
                                     <h1 class="text-3xl">
-                                       
-                                    <a href="/posts/{{ $post->slug }}" 
+
+                                    <a href="/posts/{{ $post->slug }}"
                                         {{ $post->title }}
 
-                                    </a>    
+                                    </a>
                                     </h1>
 
                                     <span class="mt-2 block text-gray-400 text-xs">
@@ -40,7 +40,7 @@
 
                             <footer class="flex justify-between items-center mt-8">
                                 <div class="flex items-center text-sm">
-                                    <img src="./images/lary-avatar.svg" alt="Lary avatar">
+                                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
                                     <div class="ml-3">
                                         <h5 class="font-bold">Lary Laracore</h5>
                                         <h6>Mascot at Laracasts</h6>
