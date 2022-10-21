a62b2e5ed0603650f566b39cbfad16f3845222e1
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-10-21 09:45:15 -0400

    added search functionality and moved get all posts route into controller file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
@@ -17,24 +19,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all()
-
-    ]);
-
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
 
 
 
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-   ]);
-
-
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 
 Route::get('categories/{category:slug}', function (Category $category) {
