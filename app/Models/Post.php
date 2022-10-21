542552eca418ecf3a9c9305426f15ca268f4ebf3
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-10-21 11:52:19 -0400

    made authors clickable within posts to view all other posts by that author

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,21 +16,34 @@ class Post extends Model
     public function scopeFilter($query, array $filters)
     {
 
-        $query->when($filters['search'] ?? false, fn($query, $search) =>
-            $query
-                ->where('title', 'like', '%' . $search . '%')
-                ->orWhere('body', 'like', '%' . $search . '%'));
+        $query->when($filters['search'] ?? false, fn ($query, $search) =>
+        $query
+            ->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%'));
 
 
-        $query->when($filters['category'] ?? false, fn($query, $category) =>
-            $query->whereExists('category', fn ($query) =>
+        $query->when(
+            $filters['category'] ?? false,
+            fn ($query, $category) =>
+            $query->whereHas(
+                'category',
+                fn ($query) =>
                 $query->where('slug', $category)
 
             )
+        );
 
-    );
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) =>
+            $query->whereHas(
+                'author',
+                fn ($query) =>
+                $query->where('username', $author)
 
+            )
 
+        );
     }
 
 
@@ -44,4 +57,3 @@ class Post extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 }
-
