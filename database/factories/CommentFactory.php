05cf61f0b3436915fe29579d73955db544e69a44
Joshua Clark <joshua.clark@HO-L6402.piedmontplastics.com>
2022-10-26 13:50:34 -0400

    updated comment factory to generate comments in connection with posts and users also added dynamic templating to pull comments from database

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +13,25 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CommentFactory extends Factory
 {
+
+
+
     /**
+     *
+     *
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
+
+
+
     public function definition()
     {
         return [
-            //
+            'post_id' => Post::factory(),
+            'user_id' => User::factory(),
+            'body' => $this->faker->paragraph()
         ];
     }
 }
