6b82aca00d61790764929bc184d8d2100446304b
Joshua Clark <joshua.clark@HO-L6402.local>
2022-11-01 16:37:14 -0400

    set destroy for route to delete blog posts in manage posts admin

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -67,4 +67,11 @@ class AdminPostController extends Controller
         return back()->with('success', 'Post Updated!');
     }
 
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return back()->with('success', 'Post Deleted!');
+    }
+
 }
